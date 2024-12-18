72a799a2398258ad9d552dbb8d7bbc178a895a1a
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2024-12-18 17:42:05 -0500

    Add pokemon model, link DB and update display pokemon. Laboratorio 8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(max_digits=6, decimal_places=4)
+    height = models.DecimalField(max_digits=6, decimal_places=4)
+
+    def __str__(self):
+        return self.name
