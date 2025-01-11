4071b2b54cf25a5ba1dde98b61502793b437bdd0
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-01-10 19:38:16 -0500

    Place trainer before pokemon in models.py

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Pokemon(models.Model):
-    name = models.CharField(max_length=30, null=False)
-    POKEMON_TYPES = {
-        ('G', 'Grass'),
-        ('W', 'Water'),
-        ('F', 'Fire'),
-        ('E', 'Electric'),
-        ('R', 'Rock'),
-    }
-    type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
-    weight = models.DecimalField(max_digits=6, decimal_places=4)
-    height = models.DecimalField(max_digits=6, decimal_places=4)
-
-    def __str__(self):
-        return self.name
-
 class Trainer(models.Model):
     first_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=30, null=False)
@@ -25,3 +9,11 @@ class Trainer(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+class Pokemon(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(max_digits=6, decimal_places=4)
+    height = models.DecimalField(max_digits=6, decimal_places=4)
+
+    def __str__(self):
+        return self.name
