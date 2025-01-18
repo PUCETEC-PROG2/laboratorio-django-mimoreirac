8d552f746f5375be7fdae6af2f467ecf0896ec5a
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-01-18 10:09:38 -0500

    Add postgresql connection

--- lab8/settings.py
+++ lab8/settings.py
@@ -76,11 +76,25 @@ WSGI_APPLICATION = 'lab8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pokedex',
+        'USER': 'postgres',
+        'PASSWORD': 'Pucetec24!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         # "NAME" : "pokedex",
+#         "OPTIONS": {
+#             "service": "pokedex",
+#             "passfile": ".pgpass",
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
